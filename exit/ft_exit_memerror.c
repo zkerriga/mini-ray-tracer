063c8007828378230e2c7d9d3cc2b6bfcf4a12cc
zkerriga <daniil.63@yandex.ru>
2020-06-20 23:35:42 +0300

    Add the exit-function

---
+++ exit/ft_exit_memerror.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit_memerror.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <zkerriga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 23:26:33 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/06/20 23:26:39 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	ft_exit_memerror(int error_code)
+{
+	errno = error_code;
+	perror("An error occurred during memory allocation");
+	exit(error_code);
+}
+
