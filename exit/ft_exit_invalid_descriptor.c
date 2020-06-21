82e1fe46af685b75792da0e425ef4834a1de968c
zkerriga <daniil.63@yandex.ru>
2020-06-21 22:21:06 +0300

    Exit update

--- exit/ft_exit_invalid_descriptor.c
+++ exit/ft_exit_invalid_descriptor.c
@@ -1,24 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*   ft_exit_invalid_descriptor.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zkerriga <zkerriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/20 23:23:50 by zkerriga          #+#    #+#             */
-/*   Updated: 2020/06/20 23:24:02 by zkerriga         ###   ########.fr       */
+/*   Created: 2020/06/21 22:11:05 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/06/21 22:11:17 by zkerriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include "libft.h"
 
-void	ft_exit(int error_code)
+void	ft_exit_invalid_descriptor()
 {
-	if (error_code == ENOMEM)
-		ft_exit_memerror(error_code);
-	else if (error_code == INVALID_INPUT)
-		ft_exit_invalid_input();
-	else if (error_code == INVALID_DESCRIPTOR)
-		ft_exit_invalid_descriptor();
+	ft_putendl_fd("The file path is incorrect!", 1);
+	free_gc(NULL);
+	exit(0);
 }
-
