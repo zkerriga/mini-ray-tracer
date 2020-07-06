51656fede8137443e03f64811f84ef17ad554441
zkerriga <daniil.63@yandex.ru>
2020-07-07 01:20:42 +0300

    Update CMake and add some tests

--- Google_test/color_test.cpp
+++ Google_test/color_test.cpp
@@ -24,7 +24,6 @@ void	get_bits(int n) {
 
 TEST (color, color_0)
 {
-
 	t_color color;
 
 	color.r = 224;
@@ -32,4 +31,42 @@ TEST (color, color_0)
 	color.b = 128;
 	get_bits(color_to_int(color));
 	ASSERT_EQ(0b111000000000010010000000, color_to_int(color));
+}
+
+TEST (color, color_1)
+{
+	t_color dest;
+	t_color color;
+	t_color color2;
+
+	color.r = 200;
+	color.g = 0;
+	color.b = 254;
+	color2.r = 10;
+	color2.g = 10;
+	color2.b = 10;
+	color_sum(&dest, color, color2, 1);
+	printf("->%d:%d:%d<-\n", dest.r, dest.g, dest.b);
+	ASSERT_EQ(dest.r, 210);
+	ASSERT_EQ(dest.g, 10);
+	ASSERT_EQ(dest.b, 255);
+}
+
+TEST (color, color_2)
+{
+	t_color dest;
+	t_color color;
+	t_color color2;
+
+	color.r = 200;
+	color.g = 0;
+	color.b = 254;
+	color2.r = 10;
+	color2.g = 10;
+	color2.b = 10;
+	color_sum(&dest, color, color2, 0.5);
+	printf("->%d:%d:%d<-\n", dest.r, dest.g, dest.b);
+	ASSERT_EQ(dest.r, 205);
+	ASSERT_EQ(dest.g, 5);
+	ASSERT_EQ(dest.b, 255);
 }
