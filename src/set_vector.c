eafba69f6d63cb90c86b9a3e7d2626f9ec6b3e15
zkerriga <daniil.63@yandex.ru>
2020-07-09 10:05:31 +0300

    Add the set_vector function and vdor refactor

--- src/set_vector.c
+++ src/set_vector.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   func.h                                             :+:      :+:    :+:   */
+/*   set_vector.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zkerriga <zkerriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/29 17:47:35 by zkerriga          #+#    #+#             */
-/*   Updated: 2020/06/29 17:47:49 by zkerriga         ###   ########.fr       */
+/*   Created: 2020/07/09 09:31:37 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/07/09 09:31:39 by zkerriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FUNC_H
-#define FUNC_H
-
 #include "minirt.h"
 
-int			color_to_int(t_rgbvec color);
-t_rgbvec	*color_sum(t_rgbvec *dest, t_rgbvec one, t_rgbvec two);
-t_rgbvec	*color_multi(t_rgbvec *dest, t_rgbvec one, t_rgbvec two);
-float		vdot(t_3dvector a, t_3dvector b);
-float		module(t_3dvector vector);
-t_bool		fbetween(float nbr, float down, float up);
-void		set_vector(t_3dvector *dest, t_point to, t_point from);
+/*
+** Write to `dest` vector obtained from two points.
+** Example: vec.x = to.x - from.x;
+*/
 
-#endif
+void	set_vector(t_3dvector *dest, t_point to, t_point from)
+{
+	dest->x = to.x - from.x;
+	dest->y = to.y - from.y;
+	dest->z = to.z - from.z;
+}
--- src/vdot.c
+++ src/vdot.c
@@ -12,6 +12,10 @@
 
 #include "minirt.h"
 
+/*
+** Scalar multiplication of two vectors
+*/
+
 float	vdot(t_3dvector a, t_3dvector b)
 {
 	return (a.x * b.x + a.y * b.y + a.z * b.z);
